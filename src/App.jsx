a897462b7df9605dbbd12a31614f72cede56d6fa
catharsis911 <127673819+catharsis911@users.noreply.github.com>
2024-01-28 23:18:31 +0300

    fixed components

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,6 @@ function App() {
           <Route path="/register" element={ <Register/> }/>
 
         </Routes>
-        <Search/>
         <Footer/>
       </div>
     </>
--- src/Components/TakeTest.jsx
+++ src/Components/TakeTest.jsx
@@ -4,13 +4,13 @@ const Test = () => {
     return (
         <div className="testContainer bg-white shadow-xl shadow-grayIsh hover:shadow-2xl h-[400px] w-[70%] my-20 mx-auto p-10">
           <div className="h-[70px]">
-          <h1 className="text-[30px] text-center text-textColor font-semibold">MBTI (Myers-Briggs Type Indicator)</h1>
+          <h1 className="lg:text-[30px] text-[23px] text-center text-textColor font-semibold">MBTI (Myers-Briggs Type Indicator)</h1>
           </div>
-            <p className="text-[25px] text-[#a5a6a6] border-t-4 p-4">
+            <p className="lg:text-[25px] text-[20px] text-[#a5a6a6] border-t-4 p-4">
           It is used by organizations to help individuals develop and build self-awareness and to help teams work better together.
             </p>
            
-            <button className="bg-greenColor block m-auto p-6 rounded-[10px] w-[40%] text-[20px] text-white cursor-pointer hover:bg-green-300">Take the Test</button>
+            <button className="bg-greenColor block m-auto lg:p-6 p-3 rounded-[10px] lg:w-[40%] w-[90%] text-[20px] text-white cursor-pointer hover:bg-green-300">Take the Test</button>
         </div>
     )
 }
